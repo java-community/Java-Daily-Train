28109a6fc9c157659dacbc6c5088f5d387d5df6b
mzy <maohacker@qq.com>
2019-01-30 11:10:11 +0800

    * 说明:  * 1.判断ASCII与整型区别  * 2.打印区别:  * *(1)出现双引号时,拼接字符串，而不是运算  * *(2)不出现双引号时,才是运算

--- src/com/java/ASCII_Code.java
+++ src/com/java/ASCII_Code.java
@@ -3,7 +3,11 @@ package com.java;
 /**
  * @Author: mzy
  * @Date: 2019-1-30 10:46
- * 说明:判断ASCII与整型区别,还有打印区别
+ * 说明:
+ * 1.判断ASCII与整型区别
+ * 2.打印区别:
+ * *(1)出现双引号时,拼接字符串，而不是运算
+ * *(2)不出现双引号时,才是运算
  */
 public class ASCII_Code {
     public void A(char ch){
@@ -27,11 +31,14 @@ public class ASCII_Code {
         code.A('B');
         code.A('C');
         System.out.println("注意以下区别:");
-        System.out.println("字符串相加:1+'1'+1="+1+'1'+1);
+        System.out.println("字符串拼接:1+'1'+1="+1+'1'+1);
+        System.out.print("字符串相加:1+'1'+1=");
         System.out.println(1+'1'+1);
-        System.out.println("字符串相加:1+'2'+1="+1+'2'+1);
+        System.out.println("字符串拼接:1+'2'+1="+1+'2'+1);
+        System.out.print("字符串相加:1+'2'+1=");
         System.out.println(1+'2'+1);
-        System.out.println("字符串相加:1+'A'+1="+1+'A'+1);
+        System.out.println("字符串拼接:1+'A'+1="+1+'A'+1);
+        System.out.print("字符串相加:1+'A'+1=");
         System.out.println(1+'A'+1);
 
     }
